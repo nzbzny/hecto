97a70c138f70aded53a45990b82eaae85824e46e
nzbozny <nezbozny@hotmail.com>
2023-12-24 22:03:46 -0500

    chapter 3

---
+++ src/terminal.rs
@@ -0,0 +1,81 @@
+use crate::Position;
+use std::io::{self, stdout, Write};
+use termion::event::Key;
+use termion::input::TermRead;
+use termion::raw::{IntoRawMode, RawTerminal};
+
+pub struct Size {
+    pub width: u16,
+    pub height: u16
+}
+
+pub struct Terminal {
+    size: Size,
+    _stdout: RawTerminal<std::io::Stdout>,
+}
+
+impl Terminal {
+    /// # Errors
+    /// 
+    /// Will return error if termion is not able to read the terminal size
+    pub fn init() -> Result<Self, std::io::Error> {
+        let size =  termion::terminal_size()?;
+
+        Ok(Self {
+            size: Size {
+                width: size.0,
+                height: size.1,
+            },
+            _stdout: stdout().into_raw_mode()?,
+        })
+    }
+
+    #[must_use] pub fn size(&self) -> &Size {
+        &self.size
+    }
+
+    pub fn clear_screen() {
+        print!("{}", termion::clear::All);
+    }
+    
+    pub fn clear_current_line() {
+        print!("{}", termion::clear::CurrentLine);
+    }
+
+    #[allow(clippy::cast_possible_truncation)]
+    pub fn cursor_position(position: &Position) {
+        let Position{mut x, mut y} = position;
+        x = x.saturating_add(1); // doesn't go beyond the bounds of type - in this case caps at u16::MAX
+        y = y.saturating_add(1);
+        let x = x as u16;
+        let y = y as u16;
+
+        print!("{}", termion::cursor::Goto(x, y));
+    }
+
+    pub fn cursor_hide() {
+        print!("{}", termion::cursor::Hide);
+    }
+
+    pub fn cursor_show() {
+        print!("{}", termion::cursor::Show);
+    }
+
+    /// # Errors
+    ///
+    /// Returns errors from `io::stdout::flush` <https://doc.rust-lang.org/std/io/trait.Write.html#errors-1>
+    pub fn flush() -> Result<(), std::io::Error> {
+        io::stdout().flush()
+    }
+
+    /// # Errors
+    ///
+    /// Attempting to read invalid bytes will return an error
+    pub fn read_key() -> Result<Key, std::io::Error> {
+        loop {
+            if let Some(key) = io::stdin().lock().keys().next() {
+                return key;
+            }
+        }
+    }
+}
